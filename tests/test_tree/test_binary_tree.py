da0ea474e267acd791aea7831cadf7aa6a5e7a4c
Akhtar <reachtoakhtar@gmail.com>
2020-06-12 03:11:39 +0530

    Add problem - Insert into binary tree.

---
+++ tests/test_tree/test_binary_tree.py
@@ -0,0 +1,49 @@
+import unittest
+
+from tree.binary_tree import TreeNode, inorder, insert
+
+__author__ = "akhtar"
+
+
+class TestBinaryTree(unittest.TestCase):
+    """
+    Initial tree:
+             10
+            /  \
+           11   9
+          /    / \
+         7    15  8
+    """
+    def setUp(self):
+        self.create_tree()
+
+    def create_tree(self):
+        self.root = TreeNode(10)
+        self.root.left = TreeNode(11)
+        self.root.left.left = TreeNode(7)
+        self.root.right = TreeNode(9)
+        self.root.right.left = TreeNode(15)
+        self.root.right.right = TreeNode(8)
+
+    def test_insert_into_tree(self):
+        print("TEST INSERT INTO TREE")
+        print("===========================================================")
+
+        print("Tree to operate: ")
+        print(" "*20 + "10 \n" + " "*18 + " /  \\ \n" + " "*17 + "11    9 \n" +
+              " "*16 + "/     / \\ \n" + " "*15 + "7     15  8")
+
+        print("Inorder traversal before insertion:", end=" ")
+        inorder(self.root)
+        print()
+
+        insert(self.root, 12)
+
+        print("Inorder traversal after insertion:", end=" ")
+        inorder(self.root)
+
+        print("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tree/binary_tree.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+__author__ = "akhtar"
+
+
+class TreeNode:
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def set_data(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def set_left(self, left):
+        self.left = left
+
+    def get_left(self):
+        return self.left
+
+    def set_right(self, right):
+        self.left = right
+
+    def get_right(self):
+        return self.right
+
+
+def inorder(node):
+    """ Inorder traversal of a binary tree. """
+    if not node:
+        return
+
+    inorder(node.left)
+    print(node.data, end=" ")
+    inorder(node.right)
+
+
+def insert(node, data):
+    queue = deque([])
+    queue.append(node)
+    new_node = TreeNode(data)
+
+    # Do level order traversal until we find an empty place.
+    while len(queue):
+        node = queue[0]
+        queue.popleft()
+
+        if not node.left:
+            node.left = new_node
+            break
+        else:
+            queue.append(node.left)
+
+        if not node.right:
+            node.right = new_node
+            break
+        else:
+            queue.append(node.right)
