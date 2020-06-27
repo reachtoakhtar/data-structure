caaa35569769b0a8d52297e83676cbe942c47077
Akhtar <reachtoakhtar@gmail.com>
2020-06-27 12:28:55 +0530

    Add n-ary tree node.

--- tree/nodes.py
+++ tree/nodes.py
@@ -2,6 +2,8 @@ __author__ = "akhtar"
 
 
 class BTreeNode:
+    """ Node for a Binary Tree """
+    
     def __init__(self, data=None):
         self.data = data
         self.left = None
@@ -27,3 +29,33 @@ class BTreeNode:
 
     def get_right(self):
         return self.right
+
+
+class NTreeNode:
+    """ Node for an N-ary Tree """
+    
+    def __init__(self, data=None):
+        self.data = data
+        self.first_child = None
+        self.next_sibling = None
+    
+    def __str__(self):
+        return "Node: " + str(self.data)
+    
+    def set_data(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def set_first_child(self, first_child):
+        self.first_child = first_child
+
+    def get_first_child(self):
+        return self.first_child
+
+    def set_next_sibling(self, next_sibling):
+        self.next_sibling = next_sibling
+
+    def get_next_sibling(self):
+        return self.next_sibling
---
+++ tree/problems/n_ary_tree/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "akhtar"
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
